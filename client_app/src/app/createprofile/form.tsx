21c3813dff8aea8492dcadfac0f6355a7c56ac39
Eddie2111 <tarek42223@gmail.com>
2023-09-21 23:51:29 +0600

    feat(client_app):create profile interface under progress

--- client_app/src/app/createprofile/form.tsx
+++ client_app/src/app/createprofile/form.tsx
@@ -1,14 +1,20 @@
 'use client';
 import React from 'react';
-import {Button, Card, Input} from '@nextui-org/react';
+import {Button, Card, Input, Textarea, Spacer} from '@nextui-org/react';
 import {motion} from 'framer-motion';
+import {AiOutlinePlus} from 'react-icons/ai';
+import {RxCross2} from 'react-icons/rx';
 interface IImportProps{
     dataset: string;
+}
+interface IContactList{
+    id: number;
+    link: string;
 }
 function MotionDiv({ direction, children }: { direction: string; children: JSX.Element }): JSX.Element {
     return (
       <motion.div
-        initial={{ opacity: 0, x: direction === 'left' ? -100 : 100 }}
+        initial={{ opacity: -1, x: direction === 'left' ? -100 : 100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.4 }}
       >
@@ -18,7 +24,8 @@ function MotionDiv({ direction, children }: { direction: string; children: JSX.E
   }
 export default function Form({dataset}:IImportProps):JSX.Element{
     const [count, setCount] = React.useState(0);
-
+    const [contactlist, setContactList] = React.useState<Array | []>([0]);
+    const [contactlistdata, setContactListData] = React.useState<IContactList[]>([]);
     const HandleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log('name', event.currentTarget.name.value);
@@ -26,28 +33,50 @@ export default function Form({dataset}:IImportProps):JSX.Element{
         setCount(count+1);
     }
 
-    const dott = JSON.parse(dataset.value);
+    const dott = JSON.parse(dataset.value) || {};
     return(
-        <div>
-            <Card className='w-[300px]'>
-            <form onSubmit={HandleSubmit}>
+        <div className='w-full justify-center items-center mx-auto mb-15'>
+            <Card className='w-[330px] mx-auto my-50'>
+            <form onSubmit={HandleSubmit} className='w-full p-5'>
                 {
                     // a mutlistep form basis on count
                     count === 0 && (
                         <MotionDiv direction="left">
-                        <Input label="Name" placeholder="Name" name='name'  defaultValue={dott.name || ''}/>
-                        <Input label="Email" placeholder="Email" name='email' defaultValue={dott.email || ''}/>
-                        <Button onClick={()=>setCount(count+1)} color="primary" auto> Next </Button>
+                        <p className='text-2xl font-bold mb-2'>Create Profile</p>
+                        <Input className='w-full mt-2' label="Name" placeholder="Name" name='name'  defaultValue={dott?.name || ''}/>
+                        <Input className='w-full mt-2' label="Email" placeholder="Email" name='email' defaultValue={dott?.email || ''}/>
+                        <Input className='w-full mt-2' type='date' label="Date of Birth" placeholder=" " name='dob'/>
+                        <div className='flex flex-row justify-between mt-2'>
+                            <Button className='mt-2' onClick={()=>setCount(count+1)} color="primary" auto> Next </Button>
+                        </div>
                         </MotionDiv>
                     )
                 }
                 {
                         count === 1 && (
                         <MotionDiv direction="left">
-                        <Input label="Name" placeholder="Name" name='name'  defaultValue={dott.name || ''}/>
-                        <Input label="Email" placeholder="Email" name='email' defaultValue={dott.email || ''}/>
-                        <Button onClick={()=>setCount(count-1)} color="primary" auto> Previous </Button>
-                        <Button onClick={()=>setCount(count+1)} color="primary" auto> Next </Button>
+                        <p className='text-2xl font-bold mb-2'>How to connect to you</p>
+                        <Textarea label="Tell us about you" placeholder="I like to introduce myself..." className="max-w-xs" name='about'/>
+                        <Input className='w-full mt-2' label="location" placeholder="alpha, beta" name='location'/>
+                        <div className='flex flex-row justify-between mt-2'>
+                            <label className='mt-2'>Contact</label>
+                            <Button className='mx-1 mt-[-2px]' onClick={()=>setContactList([...contactlist, contactlist.length])} color="primary" variant='transparent' auto> <AiOutlinePlus className='text-blue-600 text-2xl'/> </Button>
+                        </div>
+                        {
+                            contactlist.map((item, index)=>(
+                                <div key={index} className='flex flex-row justify-between mt-2'>
+                                    <Input className='w-full' label="Contact" placeholder="Contact" name='contact'/>
+                                    <Button className='mx-1 mt-2' onClick={()=>{
+                                        if (contactlist.length === 1) return;
+                                        setContactList(contactlist.filter((_, i)=>i !== index))}
+                                    } variant='transparent'  auto> <RxCross2 className='text-red-600 text-2xl'/> </Button>
+                                </div>
+                            ))
+                        }
+                        <div className='flex flex-row justify-between mt-2'>
+                            <Button onClick={()=>setCount(count-1)} color="primary" auto> Previous </Button>
+                            <Button onClick={()=>setCount(count+1)} color="primary" auto> Next </Button>
+                        </div>
                         </MotionDiv>
                     )
                 }
@@ -55,10 +84,12 @@ export default function Form({dataset}:IImportProps):JSX.Element{
                     (
                         count === 2 && (
                             <MotionDiv direction="left">
-                            <Input label="Name" placeholder="Name" name='name'  defaultValue={dott.name || ''}/>
-                            <Input label="Email" placeholder="Email" name='email' defaultValue={dott.email || ''}/>
-                            <Button onClick={()=>setCount(count-1)} color="primary" auto> Previous </Button>
-                            <Button type='submit' color="primary" auto> Submit </Button>
+                            <Input className='w-full mt-2' label="Name" placeholder="Name" name='name'  defaultValue={dott?.name || ''}/>
+                            <Input className='w-full mt-2' label="Email" placeholder="Email" name='email' defaultValue={dott?.email || ''}/>
+                            <div className='flex flex-row justify-between mt-2 mt-2'>
+                                <Button onClick={()=>setCount(count-1)} color="primary" auto> Previous </Button>
+                                <Button type='submit' color="primary" auto> Submit </Button>
+                            </div>
                             </MotionDiv>
                         )
                     )
@@ -67,9 +98,9 @@ export default function Form({dataset}:IImportProps):JSX.Element{
                     (
                         count === 3 && (
                             <MotionDiv direction="right">
-                                <Card className='h-20 w-[300px]'>
+                                <div className='h-20 w-[300px]'>
                                     <p>Thank you for submitting</p>
-                                </Card>
+                                </div>
                             </MotionDiv>
                         )
 
